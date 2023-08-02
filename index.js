2b65840f167bf4b48f72566074c9c7e71b4dac94
unknown <barronpeterson56@gmail.com>
2023-08-02 11:14:48 -0500

    add the questions

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquire = require('inquirer')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+    type:'input',
+    message:'What is the title of your project?',
+    name:'title',
+},
+{
+    type:'input',
+    message:'Give a pref description of your project.',
+    name:'description',
+},
+{
+    type:'input',
+    message:'How would a user install this project?',
+    name:'install',
+},
+{
+    type:'input',
+    message:'What information would you give to a user that is using this?',
+    name:'usage',
+},
+{
+    type:'input',
+    message:'What are the guidelines for someone to contribute to this?',
+    name:'contribution',
+},
+{
+    type:'input',
+    message:'How would someone go about testing your project?',
+    name:'test',
+},
+{
+    type:'list',
+    message:'What type of license do you want for your project?',
+    choices:['Mit','GPLv2','Apache','GPLv3','BSD 3-clause','BSD 2-clause','LGPLv3','None'],
+    name:'license',
+},
+{
+    type:'input',
+    message:'What is your github username?',
+    name:'username',
+},
+{
+    type:'input',
+    message:'What is your email so people can reach out if they have further questions?',
+    name:'email',
+},
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
